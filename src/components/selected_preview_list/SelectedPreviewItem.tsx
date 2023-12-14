c999b0bd6bca954989c512c598131c0d9877aa41
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-14 21:35:00 +0900

    Created adding preview function.

---
+++ src/components/selected_preview_list/SelectedPreviewItem.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+    Button,
+    ButtonIcon,
+    CloseIcon,
+    HStack,
+    Heading,
+    Image,
+    Pressable,
+    VStack,
+} from "@gluestack-ui/themed";
+import {PreviewList} from "../../global/constant";
+import {useSetRecoilState} from "recoil";
+import {SelectedPreviewState} from "../../global/recoil";
+
+export default function SelectedPreviewItem({
+    id,
+    index,
+}: {
+    id: number;
+    index: number;
+}) {
+    const setSelectedPreview = useSetRecoilState(SelectedPreviewState);
+
+    function onPressDelete() {
+        setSelectedPreview(prev => prev.filter(item => item !== id));
+    }
+
+    return (
+        <VStack space="sm">
+            <HStack px={20} justifyContent="space-between" alignItems="center">
+                <Heading>{index + 1}</Heading>
+                <Heading>{PreviewList[id - 1].title}</Heading>
+                <Button
+                    variant="link"
+                    borderRadius="$full"
+                    size="xl"
+                    onPress={onPressDelete}>
+                    <ButtonIcon as={CloseIcon} color="red" />
+                </Button>
+            </HStack>
+            <Pressable
+                w={"80%"}
+                h={250}
+                borderWidth={1}
+                borderColor="$trueGray300"
+                borderRadius={20}
+                alignSelf="center"
+                mb={20}
+                alignItems="center"
+                justifyContent="center"
+                $hover={{transform: "scale(1.05)"}}
+                overflow="hidden"
+                sx={{
+                    _web: {
+                        transition: "all 0.2s linear",
+                    },
+                }}>
+                <Image
+                    source={PreviewList[id - 1].image}
+                    w={"100%"}
+                    h={"100%"}
+                />
+            </Pressable>
+        </VStack>
+    );
+}
--- src/components/selected_preview_list/SelectedPreviewList.tsx
+++ src/components/selected_preview_list/SelectedPreviewList.tsx
@@ -1,10 +1,24 @@
 import {ScrollView} from "@gluestack-ui/themed";
 import React from "react";
 import AddPreview from "./AddPreview";
+import {SelectedPreviewState} from "../../global/recoil";
+import {useRecoilValue} from "recoil";
+import {FlatList} from "react-native";
+import SelectedPreviewItem from "./SelectedPreviewItem";
 
 export default function SelectedPreviewList() {
+    const selectedPreview = useRecoilValue(SelectedPreviewState);
+
     return (
         <ScrollView flex={1} h={"$full"} showsVerticalScrollIndicator={false}>
+            {selectedPreview.length > 0 && (
+                <FlatList
+                    data={selectedPreview}
+                    renderItem={({item, index}) => (
+                        <SelectedPreviewItem id={item} index={index} />
+                    )}
+                />
+            )}
             <AddPreview />
         </ScrollView>
     );
